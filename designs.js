3e876b23e74a01bde2e737d4a9573c36bafea2af
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-10-12 17:12:41 +0300

    grid complete

---
+++ designs.js
@@ -0,0 +1,68 @@
+// var height = document.getElementsByTagName('input')[0];
+
+// var width = document.getElementsByTagName('input')[1];
+var clickSubmit = document.querySelector(".check");
+
+clickSubmit.addEventListener("click",  submitForm)
+
+
+function submitForm(event) {
+        event.preventDefault();
+        var height = Number(document.getElementsByTagName("input")[0].value);
+        var width = Number(document.getElementsByTagName("input")[1].value);
+        console.log(height);
+        console.log(width);
+      
+        if (!height || !width) {
+          window.alert("Enter a number above zero!!!👀");
+        } else if (height > 35 || width > 35) {
+          window.alert(
+            "Please, be reasonable with your numbers in order to fit in the window pane!!! Your number should be less then 35."
+          );
+        } else if (height === 1 && width === 1) {
+          window.alert(
+            `"${height} x ${width}" is not fun!!! Enter something more creative!!!`
+          );
+        } else {
+              makeGrid();
+              clickSubmit.removeEventListener("click",  submitForm)
+        }
+      };
+
+
+function makeGrid() {
+  var body = document.getElementsByTagName("body")[0];
+  // creates a <table> element and a <tbody> element
+  var grid = document.createElement("grid");
+  var gridBody = document.createElement("gbody");
+  //creating cells
+  for (
+    var r = 0;
+    r < Number(document.getElementsByTagName("input")[0].value);
+    r++
+  ) {
+    //creates a table row
+    var row = document.createElement("tr"); //creates N of row(s)
+    for (
+      var c = 0;
+      c < Number(document.getElementsByTagName("input")[1].value);
+      c++
+    ) {
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+      var cell = document.createElement("td"); //creates N of column(s)
+      var cellText = document.createTextNode("");
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+    // add the row to the end of the grid body
+    gridBody.appendChild(row);
+  }
+  // put the gbody in the grid
+  grid.appendChild(gridBody);
+  // appends grid into body
+  body.appendChild(grid);
+  // sets the border attribut of grid to 2
+  grid.setAttribute("border", "1");
+}
