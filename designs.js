547b5ca4a84133b3a748129bda66c63f121355fb
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-10-13 16:16:25 +0300

    achieved the desirede functionality

--- designs.js
+++ designs.js
@@ -1,38 +1,38 @@
 var clickSubmit = document.querySelector(".check");
 // adding .addEventListener to "submit" button:
-clickSubmit.addEventListener("click",  submitForm)
-
+clickSubmit.addEventListener("click", submitForm);
 
 //"submitForm" should be a separate function, because in case .removeEventListener needs to be added it should refer to the same exact "submitForm" function:
 function submitForm(event) {
-        event.preventDefault();
-        var height = Number(document.getElementsByTagName("input")[0].value);
-        var width = Number(document.getElementsByTagName("input")[1].value);
-        console.log(height);
-        console.log(width);
-      
-        if (!height || !width) {
-          window.alert("Enter a number above zero!!!👀");
-        } else if (height >= 35 || width >= 35) {
-          window.alert(
-            "Please, be reasonable with your numbers in order to fit in the window pane!!! Your number should be less then 35."
-          );
-        } else if (height === 1 && width === 1) {
-          window.alert(
-            `"${height} x ${width}" is not fun!!! Enter something more creative!!!`
-          );
-        } else {
-              makeGrid();
-              clickSubmit.removeEventListener("click",  submitForm)       //preventing makeGrid() from adding a new grid to existing grid 
-        }
-      };
+  event.preventDefault();
+  var height = Number(document.getElementsByTagName("input")[0].value);
+  var width = Number(document.getElementsByTagName("input")[1].value);
+  console.log(height);
+  console.log(width);
 
+  if (!height || !width) {
+    window.alert("Enter a number above zero!!!👀");
+  } else if (height >= 35 || width >= 35) {
+    window.alert(
+      "Please, be reasonable with your numbers in order to fit in the window pane!!! Your number should be less then 35."
+    );
+  } else if (height === 1 && width === 1) {
+    window.alert(
+      `"${height} x ${width}" is not fun!!! Enter something more creative!!!`
+    );
+  } else {
+    makeGrid();
+    clickSubmit.removeEventListener("click", submitForm); //preventing makeGrid() from adding a new grid to existing grid
+  }
+}
+
+var gridBody = document.getElementById('pixelCanvas');
 
 function makeGrid() {
   var body = document.getElementsByTagName("body")[0];
   // creates a <table> element and a <tbody> element
   var grid = document.createElement("grid");
-  var gridBody = document.createElement("gbody");
+  // var gridBody = document.createElement("gbody");
   //creating cells
   for (
     var r = 0;
@@ -66,26 +66,40 @@ function makeGrid() {
 }
 
 
-// var colorPicker = document.getElementById('colorPicker');
-// var allTdElements = document.getElementsByTagName('td');
+//add event listener to update the color:
+const picker = document.getElementById('colorPicker')
+let color = picker.value;
 
-// colorPicker.addEventListener('click', setColor)
+picker.onchange = function() {
+  color = this.value;
+}
 
-// //"setColor" should be a separate function, because in case .removeEventListener needs to be added it should refer to the same exact "setColor" function:
+// function activated when user click on only
 
-// function setColor(event){
-  
-//     window.alert('you clicked');
-// }
+function respondToClick(event) {
+  if (event.target.nodeName.toLowerCase() === 'td') {
+    event.target.style.backgroundColor = color;
+  }
+}
 
-// setColor()
+gridBody.addEventListener("click", respondToClick);
 
 
+// var allTdElements = document.getElementsByTagName("td");
+//test:
+// function getAccess(){
+//   for (var i = 0; i< allTdElements.length; i++){
+//       console.log(allTdElements[i])}};
 
-
-// function updateAll(event) {
-//     document.querySelectorAll("td").forEach(function(td) {
-//       td.style.color = event.target.colorPicker;
-//     });
+// function getAccess() {
+//   for (var i = 0; i < allTdElements.length; i++) {
+//     allTdElements[i].addEventListener('click',function(){
+//       console.log(allTdElements[1].style.colorPicker)
+//     })
 //   }
+// }
+// getAccess();
+
+
+
 
